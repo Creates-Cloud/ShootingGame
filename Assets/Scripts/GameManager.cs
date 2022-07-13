80c70783df232d9f108505b345f1c202661e5ba0
UchidaEditorial <kouei28poiou@icloud.com>
2022-07-13 22:22:30 +0900

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour 
+{
 
 
     public float speed = 5.0F;
@@ -15,13 +16,16 @@ public class GameManager : MonoBehaviour {
     private List<GameObject> ammoList = new List<GameObject>();
     private bool shooted;
 
-    void Start() {
+    void Start() 
+    {
         Application.targetFrameRate = 60;
         StartCoroutine(this.StartDance());
     }
 
-    void FixedUpdate() {
-        if (playerTrans != null) {
+    void FixedUpdate() 
+    {
+        if (playerTrans != null) 
+        {
             Transform trans = this.playerTrans;
             bool right = Input.GetKey(KeyCode.RightArrow);
             bool left = Input.GetKey(KeyCode.LeftArrow);
@@ -29,8 +33,10 @@ public class GameManager : MonoBehaviour {
             float deltaSpeed = this.speed * Time.deltaTime;
             trans.Translate(right && left ? 0 : right ? deltaSpeed : left ? -deltaSpeed : 0, 0, 0);
 
-            foreach (GameObject ammo in this.ammoList) {
-                if (Vector3.Distance(trans.position, ammo.transform.position) >= 10) {
+            foreach (GameObject ammo in this.ammoList)
+            {
+                if (Vector3.Distance(trans.position, ammo.transform.position) >= 10)
+                {
                     this.ammoList.Remove(ammo);
                     GameObject.Destroy(ammo);
                     continue;
@@ -39,7 +45,8 @@ public class GameManager : MonoBehaviour {
                 ammo.transform.Translate(0, this.ammoSpeed * Time.deltaTime, 0);
             }
 
-            if (space && !shooted) {
+            if (space && !shooted) 
+            {
                 this.ShootAmmo();
                 this.resources.audioSource.PlayOneShot(this.resources.shootSound, 0.5F);
             }
@@ -48,8 +55,10 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    void ShootAmmo() {
-        if (this.ammoPrefab != null) {
+    void ShootAmmo() 
+    {
+        if (this.ammoPrefab != null) 
+        {
             GameObject obj = GameObject.Instantiate(this.ammoPrefab, this.playerTrans.position, Quaternion.identity);
             SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
             spriteRenderer.sortingOrder = 1;
@@ -60,8 +69,10 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    private IEnumerator StartDance() {
-        while (this.backGroundSpriteReanderer != null && this.backGroundSpriteReanderer.gameObject.activeSelf) {
+    private IEnumerator StartDance() 
+    {
+        while (this.backGroundSpriteReanderer != null && this.backGroundSpriteReanderer.gameObject.activeSelf) 
+        {
             this.backGroundSpriteReanderer.flipX = !this.backGroundSpriteReanderer.flipX;
             yield return new WaitForSeconds(0.5F);
         }
